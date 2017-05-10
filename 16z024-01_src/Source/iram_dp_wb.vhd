e16e307a0a7cc87e44605f0364e6cdf160c94fdb
Grzegorz Daniluk <grzegorz.daniluk@cern.ch>
2017-05-10 08:54:00 +0200

    core-generation: 16z024-01:replace generated RAM with manual instances of altsyncram

--- 16z024-01_src/Source/iram_dp_wb.vhd
+++ 16z024-01_src/Source/iram_dp_wb.vhd
@@ -45,6 +45,9 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 
+library altera_mf;
+use altera_mf.altera_mf_components.all;
+
 ENTITY iram_dp_wb IS
 GENERIC
 (
@@ -82,50 +85,6 @@ PORT
 END iram_dp_wb;
 
 ARCHITECTURE iram_dp_wb_arch OF iram_dp_wb IS 
-COMPONENT iram_dp_2clk
-	GENERIC
-	(
-	   USEDW_WIDTH	: positive := 6								-- width of address vector (6 = one M4K)
-	);
-	PORT
-	(
-		data_a		: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
-		wren_a		: IN STD_LOGIC  := '1';
-		address_a		: IN STD_LOGIC_VECTOR (USEDW_WIDTH-1 DOWNTO 0);
-		data_b		: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
-		address_b		: IN STD_LOGIC_VECTOR (USEDW_WIDTH-1 DOWNTO 0);
-		wren_b		: IN STD_LOGIC  := '1';
-		byteena_a		: IN STD_LOGIC_VECTOR (3 DOWNTO 0) :=  (OTHERS => '1');
-		byteena_b		: IN STD_LOGIC_VECTOR (3 DOWNTO 0) :=  (OTHERS => '1');
-		clock_a		: IN STD_LOGIC ;
-		clock_b		: IN STD_LOGIC ;
-		q_a		: OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
-		q_b		: OUT STD_LOGIC_VECTOR (31 DOWNTO 0)
-	);
-END COMPONENT;
-
-COMPONENT iram_dp_1clk
-	GENERIC
-	(
-	   USEDW_WIDTH	: positive := 6								-- width of address vector (6 = one M4K)
-	);
-	PORT
-	(
-		data_a		: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
-		wren_a		: IN STD_LOGIC  := '1';
-		address_a		: IN STD_LOGIC_VECTOR (USEDW_WIDTH-1 DOWNTO 0);
-		data_b		: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
-		address_b		: IN STD_LOGIC_VECTOR (USEDW_WIDTH-1 DOWNTO 0);
-		wren_b		: IN STD_LOGIC  := '1';
-		byteena_a		: IN STD_LOGIC_VECTOR (3 DOWNTO 0) :=  (OTHERS => '1');
-		byteena_b		: IN STD_LOGIC_VECTOR (3 DOWNTO 0) :=  (OTHERS => '1');
-		clock		: IN STD_LOGIC ;
-		q_a		: OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
-		q_b		: OUT STD_LOGIC_VECTOR (31 DOWNTO 0)
-	);
-END COMPONENT;
-
-
 
 	SIGNAL sl0_loc_be			: std_logic_vector(3 DOWNTO 0);
 	SIGNAL sl0_ack_o_int		: std_logic;
@@ -198,52 +157,109 @@ END PROCESS sl1;
 gen_2clk: IF NOT SAME_CLK GENERATE
 	sl0_clk_int <= sl0_clk;
 	sl1_clk_int <= sl1_clk;
+
+	altsyncram_component : altsyncram
+	GENERIC MAP (
+		intended_device_family => "Cyclone",
+		operation_mode => "BIDIR_DUAL_PORT",
+		width_a => 32,
+		widthad_a => USEDW_WIDTH,
+		numwords_a => 2**USEDW_WIDTH,
+		width_b => 32,
+		widthad_b => USEDW_WIDTH,
+		numwords_b => 2**USEDW_WIDTH,
+		lpm_type => "altsyncram",
+		width_byteena_a => 4,
+		width_byteena_b => 4,
+		byte_size => 8,
+		outdata_reg_a => "UNREGISTERED",
+		outdata_aclr_a => "NONE",
+		outdata_reg_b => "UNREGISTERED",
+		indata_aclr_a => "NONE",
+		wrcontrol_aclr_a => "NONE",
+		address_aclr_a => "NONE",
+		byteena_aclr_a => "NONE",
+		indata_reg_b => "CLOCK1",
+		address_reg_b => "CLOCK1",
+		wrcontrol_wraddress_reg_b => "CLOCK1",
+		indata_aclr_b => "NONE",
+		wrcontrol_aclr_b => "NONE",
+		address_aclr_b => "NONE",
+		byteena_reg_b => "CLOCK1",
+		byteena_aclr_b => "NONE",
+		outdata_aclr_b => "NONE",
+		power_up_uninitialized => "FALSE",
+		init_file => "iram.hex"
+	)
+	PORT MAP (
+		clock0    => sl0_clk,
+		wren_a    => sl0_write,
+		byteena_a => sl0_loc_be,
+		address_a => sl0_adr(USEDW_WIDTH+1 DOWNTO 2),
+		data_a    => sl0_dat_i,
+		q_a       => sl0_dat_o,
+
+		clock1    => sl1_clk,
+		wren_b    => sl1_write,
+		byteena_b => sl1_loc_be,
+		address_b => sl1_adr(USEDW_WIDTH+1 DOWNTO 2),
+		data_b    => sl1_dat_i,
+		q_b       => sl1_dat_o);
 	
-	iram_2c: iram_dp_2clk
-		GENERIC MAP
-			(
-		   USEDW_WIDTH		=> USEDW_WIDTH
-			)
-		PORT MAP 
-		(
-			data_a			=> sl0_dat_i,
-			wren_a			=> sl0_write,
-			address_a		=> sl0_adr(USEDW_WIDTH+1 DOWNTO 2),
-			data_b			=> sl1_dat_i,
-			address_b		=> sl1_adr(USEDW_WIDTH+1 DOWNTO 2),
-			wren_b			=> sl1_write,
-			byteena_a		=> sl0_loc_be,
-			byteena_b		=> sl1_loc_be,
-			clock_a			=> sl0_clk,
-			clock_b			=> sl1_clk,
-			q_a				=> sl0_dat_o,
-			q_b				=> sl1_dat_o
-		);
-	END GENERATE gen_2clk;
+END GENERATE gen_2clk;
 	
 gen_1clk: IF SAME_CLK GENERATE
 	sl0_clk_int <= sl0_clk;
 	sl1_clk_int <= sl0_clk;
 
-	iram_1c: iram_dp_1clk
-		GENERIC MAP
-			(
-		   USEDW_WIDTH		=> USEDW_WIDTH
-			)
-		PORT MAP 
-		(                    
- 			data_a			=> sl0_dat_i,  
-			wren_a			=> sl0_write,     
-			address_a		=> sl0_adr(USEDW_WIDTH+1 DOWNTO 2),    
-			data_b			=> sl1_dat_i,  
-			address_b		=> sl1_adr(USEDW_WIDTH+1 DOWNTO 2),    
-			wren_b			=> sl1_write,     
-			byteena_a		=> sl0_loc_be, 
-			byteena_b		=> sl1_loc_be, 
-			clock				=> sl0_clk,
-			q_a				=> sl0_dat_o, 
-			q_b				=> sl1_dat_o  
-		);
-	END GENERATE gen_1clk;
+	altsyncram_component : altsyncram
+	GENERIC MAP (
+		intended_device_family => "Cyclone",
+		ram_block_type => "M4K",
+		operation_mode => "BIDIR_DUAL_PORT",
+		width_a => 32,
+		widthad_a => USEDW_WIDTH,
+		numwords_a => 2**USEDW_WIDTH,
+		width_b => 32,
+		widthad_b => USEDW_WIDTH,
+		numwords_b => 2**USEDW_WIDTH,
+		lpm_type => "altsyncram",
+		width_byteena_a => 4,
+		width_byteena_b => 4,
+		byte_size => 8,
+		outdata_reg_a => "UNREGISTERED",
+		outdata_aclr_a => "NONE",
+		outdata_reg_b => "UNREGISTERED",
+		indata_aclr_a => "NONE",
+		wrcontrol_aclr_a => "NONE",
+		address_aclr_a => "NONE",
+		byteena_aclr_a => "NONE",
+		indata_reg_b => "CLOCK0",
+		address_reg_b => "CLOCK0",
+		wrcontrol_wraddress_reg_b => "CLOCK0",
+		indata_aclr_b => "NONE",
+		wrcontrol_aclr_b => "NONE",
+		address_aclr_b => "NONE",
+		byteena_reg_b => "CLOCK0",
+		byteena_aclr_b => "NONE",
+		outdata_aclr_b => "NONE",
+		read_during_write_mode_mixed_ports => "OLD_DATA",
+		power_up_uninitialized => "FALSE",
+		init_file => "iram.hex")
+	PORT MAP (
+		clock0 => sl0_clk,
+
+		wren_a    => sl0_write,
+		byteena_a => sl0_loc_be,
+		address_a => sl0_adr(USEDW_WIDTH+1 DOWNTO 2),
+		data_a    => sl0_dat_i,
+		q_a       => sl0_dat_o, 
+		wren_b    => sl1_write,
+		byteena_b => sl1_loc_be,
+		address_b => sl1_adr(USEDW_WIDTH+1 DOWNTO 2),
+		data_b    => sl1_dat_i,
+		q_b       => sl1_dat_o);
+
+END GENERATE gen_1clk;
 
 END iram_dp_wb_arch;
